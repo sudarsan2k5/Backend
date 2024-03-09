a45a784ab355a39669b4d76fa305c2c4c1272009
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2024-03-09 15:54:53 +0530

    Read file With Streams

--- Streams/steams.js
+++ Streams/steams.js
@@ -1,16 +1,34 @@
 // Now lets say that for some reason in our application, we need to read a large text file from the file and then send to the client.
 
+
+            /////---------SOLUTION 1---------/////////
+
+// const fs = require('fs');
+// const server = require('http').createServer();
+
+// server.on('request', (req, res) => {
+//     // Solution 1
+//     fs.readFile('./index.text', (err, data) => {
+//         if(err) console.log("Error");
+//         res.end(data);
+//     });
+// });
+
+
+            /////////----------SOLUTION 2--------///////
 const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-    // Solution 1
-    fs.readFile('./index.text', (err, data) => {
-        if(err) console.log("Error");
-        res.end(data);
+    const readable = fs.createReadStream('index.text');
+    readable.on('data', (chunkData) => {
+        res.write(chunkData);
     });
+    readable.on('end', () =>{
+        res.end()
+    })
 });
 
 server.listen('8000', '127.0.0.1', () => {
-    console.log("Listing......");````
+    console.log("Listing.....");
 });
